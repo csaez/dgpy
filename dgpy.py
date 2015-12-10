09f54643ae7072d4250ebadf4afb9eb588b32625
csaez <cesarte@gmail.com>
2015-12-10 21:49:39 +1100

    AddNode implementation

--- dgpy.py
+++ dgpy.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+
 class Graph(object):
     def __init__(self):
         super(Graph, self).__init__()
@@ -12,6 +15,53 @@ class Graph(object):
         self.nodes.remove(node)
 
 
+class Port(object):
+    def __init__(self, name):
+        super(Port, self).__init__()
+        self.name = name
+        self.value = None
+
+
 class VoidNode(object):
     def __init__(self):
         super(VoidNode, self).__init__()
+        self.inputPorts = OrderedDict()
+        self.outputPorts = OrderedDict()
+        self.initPorts()
+
+    def initPorts(self):
+        pass
+
+    def getInputPort(self, name):
+        return self.inputPorts.get(name)
+
+    def getOutputPort(self, name):
+        return self.outputPorts.get(name)
+
+    def addInputPort(self, name):
+        port = Port(name)
+        self.inputPorts[name] = port
+
+    def addOutputPort(self, name):
+        port = Port(name)
+        self.outputPorts[name] = port
+
+    def evaluate(self):
+        pass
+
+
+class AddNode(VoidNode):
+    def __init__(self):
+        super(AddNode, self).__init__()
+
+    def initPorts(self):
+        super(AddNode, self).initPorts()
+        self.addInputPort("value1")
+        self.addInputPort("value2")
+        self.addOutputPort("result")
+
+    def evaluate(self):
+        super(AddNode, self).evaluate()
+        result = sum([p.value for p in self.inputPorts.values()
+                      if p.value is not None])
+        self.getOutputPort("result").value = result
--- tests.py
+++ tests.py
@@ -10,5 +10,14 @@ class UsageCase(unittest.TestCase):
         graph.removeNode(node)
         self.assertEqual(len(graph.nodes), 0)
 
+    def testAddNode(self):
+        graph = dgpy.Graph()
+        node = graph.addNode(dgpy.AddNode)
+        node.getInputPort("value1").value = 2
+        node.getInputPort("value2").value = 3
+        node.evaluate()
+        self.assertEqual(node.getOutputPort("result").value, 2+3)
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
