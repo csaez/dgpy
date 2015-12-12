f18e2d2c5fe096d3fb1f88bf7b8abdeda8ebc03e
csaez <cesarte@gmail.com>
2015-12-12 16:23:16 +1100

    Fix connections under push model

--- dgpy.py
+++ dgpy.py
@@ -37,14 +37,6 @@ class Port(object):
         self.isConnected = False
         self.dataSource = None
 
-    def connect(self, outputPort):
-        self.isConnected = True
-        self.dataSource = outputPort
-        self.updateValue()
-
-    def updateValue(self):
-        self.value = self.dataSource.value
-
     def getValue(self):
         return self._value
 
@@ -56,6 +48,20 @@ class InputPort(Port):
     def setValue(self, value):
         super(InputPort, self).setValue(value)
         self.owner.evaluate()
+
+    def connect(self, outputPort):
+        self.isConnected = True
+        outputPort.isConnected = True
+        self.dataSource = outputPort
+        outputPort.dataSource = self
+        self.value = outputPort.value
+
+
+class OutputPort(Port):
+    def setValue(self, value):
+        if self.isConnected:
+            self.dataSource.value = value
+        super(OutputPort, self).setValue(value)
 
 
 class VoidNode(object):
@@ -81,15 +87,12 @@ class VoidNode(object):
         self.inputPorts[name] = port
 
     def addOutputPort(self, name):
-        port = Port(name)
+        port = OutputPort(name)
         port.owner = self
         self.outputPorts[name] = port
 
     def evaluate(self):
         logger.debug("Evaluating {}".format(self))
-        for p in self.inputPorts.values():
-            if p.isConnected:
-                p.updateValue()
 
 
 class AddNode(VoidNode):
--- tests.py
+++ tests.py
@@ -4,46 +4,41 @@ import logging
 
 
 class UsageCase(unittest.TestCase):
-    def testBaseClasses(self):
+    def testAddRemoveNodes(self):
         graph = dgpy.Graph()
         node = graph.addNode("node", dgpy.VoidNode)
         self.assertEqual(len(graph.nodes), 1)
         graph.removeNode(node)
         self.assertEqual(len(graph.nodes), 0)
 
-    def testAddNode(self):
+    def testSingleNodeEvaluation(self):
         graph = dgpy.Graph()
-        node = graph.addNode("node", dgpy.AddNode)
+        node = graph.addNode("node1", dgpy.AddNode)
         node.getInputPort("value1").value = 2
         node.getInputPort("value2").value = 3
         self.assertEqual(node.getOutputPort("result").value, 2+3)
+        return graph
 
-    # def testAddNodeConnections(self):
-        # graph = dgpy.Graph()
+    def testNodeConnections(self):
+        graph = self.testSingleNodeEvaluation()
 
-        # node1 = graph.addNode("node1", dgpy.AddNode, value1=5, value2=10)
-        # node1.evaluate()
+        node1 = graph.getNode("node1")
 
-        # node2 = graph.addNode("node2", dgpy.AddNode, value1=5)
-        # node2.getInputPort("value2").connect(node1.getOutputPort("result"))
-        # node2.evaluate()
+        node2 = graph.addNode("node2", dgpy.AddNode, value1=5)
+        node2.getInputPort("value2").connect(node1.getOutputPort("result"))
+        self.assertEqual(node2.getOutputPort("result").value, 5 + 5)
 
-        # self.assertEqual(node2.getOutputPort("result").value, 20)
+        return graph
 
-        # return graph
+    def testPersistentConnections(self):
+        graph = self.testNodeConnections()
 
-    # def testGetNodesFromGraph(self):
-        # graph = self.testAddNodeConnections()
+        node1 = graph.getNode("node1")
+        node1.getInputPort("value1").value = 10
+        self.assertEqual(node1.getOutputPort("result").value, 10 + 3)
 
-        # node1 = graph.getNode("node1")
-        # node1.getInputPort("value1").value = 10
-        # node1.evaluate()
-        # self.assertEqual(node1.getOutputPort("result").value, 10 + 10)
-
-        # node2 = graph.getNode("node2")
-        # node2.evaluate()
-
-        # self.assertEqual(node2.getOutputPort("result").value, 25)
+        node2 = graph.getNode("node2")
+        self.assertEqual(node2.getOutputPort("result").value, 5 + 13)
 
 
 if __name__ == '__main__':
