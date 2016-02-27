caf22bcb9135158f44ebd94c661a42ff57cf9311
csaez <cesarte@gmail.com>
2016-02-27 17:05:15 +1100

    Setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="dgpy",
+    version="0.1.0",
+    py_modules=["dgpy"],
+    url="http://github.com/csaez/dgpy",
+    author="Cesar Saez",
+    author_email="hi@cesarsaez.me",
+)
